8d8fc43c2850f0da9b39d057742f1208f6076402
nggoong <nggoong@naver.com>
2022-07-31 17:57:12 +0900

    axios 설치 및 axios 인스턴스 생성, JWT token headers에 넣는 로직 구현

---
+++ src/shared/axios.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+
+const instance = axios.create({
+    baseURL:process.env.REACT_APP_API_URL // .env 바꾸기
+})
+
+
+instance.interceptors.request.use((config) => {
+    const token = localStorage.getItem('Authorization');
+
+    if(!token) {
+        config.headers.common['Authorization'] = null;
+        return config;
+    }
+    else {
+        config.headers.common['Authorization'] = `${token}`;
+        return config;
+    }
+})
+
+export default instance;
